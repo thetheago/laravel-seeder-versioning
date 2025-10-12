9c380410c751f7309a14ea16568b6a6e0dd599c6
Thiago <theago.dev@gmail.com>
2025-10-12 19:09:19 -0300

    feat: creating command to migrate seeders.

--- src/Command/SeedVersionedCommand.php
+++ src/Command/SeedVersionedCommand.php
@@ -2,7 +2,19 @@
 
 namespace Thetheago\SeederVersioning\Command;
 
-class SeedVersionedCommand
-{
+use Illuminate\Console\Command;
+use Thetheago\SeederVersioning\Facades\SeederVersioning;
 
+class SeedVersionedCommand extends Command
+{
+    protected $signature = 'seed:migrate';
+    protected $description = 'Run seeders that have been modified.';
+
+    public function handle(): int
+    {
+        $this->info('Running versioned seeders...');
+        SeederVersioning::runVersionedSeeders();
+        $this->info('Done.');
+        return self::SUCCESS;
+    }
 }
